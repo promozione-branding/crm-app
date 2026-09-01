30dab283dbb0a342629a55f390303ac37d7dd818
Anshuman <anshutyagi799@gmail.com>
2026-09-01 15:55:33 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
-  experimental: {
-    instrumentationHook: true,
-  },
 };
 
 export default nextConfig;
