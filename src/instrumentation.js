fc149261d2cc043da6dd2d2bed3fc2e744e59bcb
ibvijdey <bijay@inquirybazaar.com>
2026-09-23 14:42:22 +0530

    commented the instumentation.js as we need more thing

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,44 +1,44 @@
-export async function register() {
-    if (process.env.NEXT_RUNTIME !== 'nodejs') {
-        return;
-    }
+// export async function register() {
+//     if (process.env.NEXT_RUNTIME !== 'nodejs') {
+//         return;
+//     }
 
-    console.log('========================================');
-    console.log('🔍 ENVIRONMENT CHECK');
-    console.log('========================================');
+//     console.log('========================================');
+//     console.log('🔍 ENVIRONMENT CHECK');
+//     console.log('========================================');
 
-    console.log(
-        'YOUR_EMAIL_ADDRESS:',
-        process.env.YOUR_EMAIL_ADDRESS || '❌ NOT FOUND'
-    );
+//     console.log(
+//         'YOUR_EMAIL_ADDRESS:',
+//         process.env.YOUR_EMAIL_ADDRESS || '❌ NOT FOUND'
+//     );
 
-    console.log(
-        'YOUR_APP_PASSWORD:',
-        process.env.YOUR_APP_PASSWORD
-            ? '✅ FOUND'
-            : '❌ NOT FOUND'
-    );
+//     console.log(
+//         'YOUR_APP_PASSWORD:',
+//         process.env.YOUR_APP_PASSWORD
+//             ? '✅ FOUND'
+//             : '❌ NOT FOUND'
+//     );
 
-    console.log('========================================');
+//     console.log('========================================');
 
-    if (process.env.NODE_ENV !== 'development') {
-        return;
-    }
+//     if (process.env.NODE_ENV !== 'development') {
+//         return;
+//     }
 
-    const { testMail } =
-        await import('./lib/mail/reminderMail.js');
+//     const { testMail } =
+//         await import('./lib/mail/reminderMail.js');
 
-    try {
-        await testMail();
-    } catch (error) {
-        console.error(
-            'SMTP TEST FAILED:',
-            error.message
-        );
-    }
+//     try {
+//         await testMail();
+//     } catch (error) {
+//         console.error(
+//             'SMTP TEST FAILED:',
+//             error.message
+//         );
+//     }
 
-    const { startReminderCron } =
-        await import('./lib/cron/reminderCron.js');
+//     const { startReminderCron } =
+//         await import('./lib/cron/reminderCron.js');
 
-    startReminderCron();
-}
+//     startReminderCron();
+// }
